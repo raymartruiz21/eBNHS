24a5b2106f4782f3bc32aaf2bdfcfdadcd09f3a3
Raymart <raymartruiz14@gmail.com>
2021-09-12 13:17:17 -0700

    new add

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,23 +2,50 @@
 
 @section('content')
 
-<div class="container">
-    <form method="POST" action="{{ route('auth.login_post') }}" autocomplete="off">
-        @csrf
-        @if (session()->has('msg'))
-        <div class="alert alert-warning text-center" role="alert">
-            {{ session('msg') }}
-        </div>
-        @endif
-        <div class="form-group">
-            <label >Username | ID</label>
-            <input type="text" name="get_your_input" class="form-control" required >
+<div class="container py-4">
+  <form method="POST" action="{{ route('auth.login_post') }}" autocomplete="off">
+      <div class="row justify-content-center">
+          <div class="col-md-8">
+              <div class="card">
+                  <div class="card-header" style="background-color: #b3fff0;"><h4>{{ __('Login') }}</h4></div>
+                  <div class="card-body">
+                    @csrf
+                    <div class="row justify-content-center">
+                      @if (session()->has('msg'))
+                      <div class="col-md-12">
+                        <div class="alert alert-warning text-center" role="alert">{{ session('msg') }}</div>
+                      </div>
+                      @endif
+                      <div class="col-md-8">
+                        <div class="form-group mb-1">
+                            <label for="get_your_input" class="col-form-label text-md-right">Username | ID</label>
+                            <input id="get_your_input" type="text" class="form-control" name="get_your_input" value="{{ old('get_your_input') }}" required>
+                        </div>
+                      </div>
+                      <div class="col-md-8">
+                        <div class="form-group mb-1">
+                            <label for="username" class="col-form-label text-md-right">Password</label>
+                            <input type="password" name="password" class="form-control" required>
+                        </div>
+                      </div>
+                      <div class="col-md-6 text-center">
+                        <div class="form-group py-4">
+                          <button type="submit" class="btn btn-primary btn-block">Submit</button>
+                        </div>
+                      </div>
+                      {{-- <div class="form-group">
+                      <label >Username | ID</label>
+                      <input type="text" name="get_your_input" class="form-control" required >
+                      </div>
+                      <div class="form-group">
+                      <label >Password</label>
+                      <input type="password" name="password" class="form-control" required>
+                      </div>
+                      <button type="submit" class="btn btn-primary">Submit</button> --}}
+                    </div>
+              </div>
           </div>
-          <div class="form-group">
-            <label >Password</label>
-            <input type="password" name="password" class="form-control" required>
-          </div>
-          <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+      </div>
+  </form>
 </div>
 @endsection
--- resources/views/teacher/dashboard.blade.php
+++ resources/views/teacher/dashboard.blade.php
@@ -1,9 +1,7 @@
 <h1>TEACHER : {{  auth()->user()->fullname  }}</h1>
 {{-- mapagal amg code kaya tigcopy ko lng so sako --}}
-<a href="{{ route('auth.logout') }}"
-                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
-              > Logout
-                </a>
-                <form id="logout-form" action="{{ route('auth.logout') }}" method="POST" >
-                    @csrf
-                </form>
+<a href="{{ route('auth.logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"> Logout
+</a>
+<form id="logout-form" action="{{ route('auth.logout') }}" method="POST" >
+    @csrf
+</form>
